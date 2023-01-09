9b671c1a83bbaebe9d3a12e23fc6ff02aacbc53e
rus <ruslan.cheremisin@gmail.com>
2023-01-10 01:45:08 +0200

    initial commit

---
+++ src/main/java/sky/pro/skyprotermwork3/SkyproTermwork3Application.java
@@ -0,0 +1,12 @@
+package sky.pro.skyprotermwork3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTermwork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproTermwork3Application.class, args);
+    }
+}
